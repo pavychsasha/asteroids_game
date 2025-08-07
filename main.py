371af8e24654eed0d634afa89b1eacda6441c595
pavychsasha <sashpavych@gmail.com>
2025-08-06 22:50:00 -0500

    Making a ship and movement logic for it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,17 +8,31 @@ def main():
     print("Screen height:", constants.SCREEN_HEIGHT)
 
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode(
+        (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
+    )
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
+
+    player = Player(
+        x = constants.SCREEN_WIDTH / 2,
+        y = constants.SCREEN_HEIGHT / 2
+    )
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        player.update(dt)
+
         screen.fill("black")
 
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
