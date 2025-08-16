2b5221140d50b83d7e1efc1cea90eef922af7ebf
pavychsasha <pavychsasha@users.noreply.github.com>
2025-08-16 19:14:10 +0000

    Deploying to gh-pages from @ pavychsasha/asteroids_game@8807c57ef69d556ebf7e557534da074f3f2308f0 🚀

--- core/containers.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-drawable = pygame.sprite.Group()
-updatable = pygame.sprite.Group()
-asteroids = pygame.sprite.Group()
-shots = pygame.sprite.Group()
--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["CircleShape", "Player", "Asteroid", "AsteroidField", "Bullet"]
-
-from .circleshape import CircleShape
-from .bullet import Bullet
-from .player import Player
-from .asteroid import Asteroid
-from .asteroidfield import AsteroidField
